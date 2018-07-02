479fe09cb84a1c8af4747f234f06598c92b44d8a
Harold Perez <haroldjose2903@gmail.com>
2018-07-02 17:59:51 -0500

    Added factory bot methods to rspec. Tests added to rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
   end
+
+  context "have many questions" do
+    it { should have_many(:questions) }
+  end
+
+  context "have many answers" do
+    it { should have_many(:answers) }
+  end
+
+  context "without first_name or last_name" do
+    it "is not valid without first_name" do
+      user = build(:user, first_name: nil)
+      expect(user).to_not be_valid
+    end
+
+    it "is not valid without last_name" do
+      user = build(:user, last_name: nil)
+      expect(user).to_not be_valid
+    end
+  end
 end
