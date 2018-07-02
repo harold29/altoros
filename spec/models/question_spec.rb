79a94075b701e843f46c64be9b46503030c0da78
Harold Perez <haroldjose2903@gmail.com>
2018-07-02 17:30:54 -0500

    Tests for validation added

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with content" do
+    should validate_presence_of(:content)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with first name and last name" do
+    should validate_presence_of(:first_name)
+    should validate_presence_of(:last_name)
+  end
 end
