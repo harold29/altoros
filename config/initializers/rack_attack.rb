edbb66f5d47686677c682b26e0d97fede59613ad
Harold Perez <haroldjose2903@gmail.com>
2018-07-09 14:04:23 -0500

    Add hapi_key headers to rack attack initializer

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,9 @@
 class Rack::Attack
   throttle('limit logins per email', limit: 100, period: 86400) do |req|
-    req.params['api_key']
+    req.params['api_key'] || req.get_header('HTTP_API_KEY')
   end
 
   throttle('limit logins per email', limit: 1, period: 10) do |req|
-    req.params['api_key']
+    req.params['api_key'] || req.get_header('HTTP_API_KEY')
   end
 end
