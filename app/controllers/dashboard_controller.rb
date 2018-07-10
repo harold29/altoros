abed13798fe0461eabf0afe9524a5347014db59e
Harold Perez <haroldjose2903@gmail.com>
2018-07-09 22:28:22 -0500

    test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ActionController::Base
   def index
-    @u_size = User.count
-    @q_size = Question.count
+    # @u_size = User.count
+    # @q_size = Question.count
     @a_size = Answer.count
     @r_num = Tenant.sum(:successful_requests) + Tenant.sum(:failed_requests)
   end
